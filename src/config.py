96b7d9434c4e254c4547be978c6344347217de28
Dr Neeraja <ajithkannampara@gmail.com>
2024-08-23 05:12:05 +0530

    first commit

--- src/config.py
+++ src/config.py
@@ -1,4 +1,3 @@
-OPEN_AI_API = "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"
 FAISS_INDEXER_DIR = ""
 TEMP_DATA_DIR = "./temp_data"
 EMBD_MODEL_NAME = "sentence-transformers/paraphrase-albert-small-v2"
