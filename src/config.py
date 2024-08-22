67f9ff680e2e1bffd57c9e1ff5b9aef67c642d4f
Dr Neeraja <ajithkannampara@gmail.com>
2024-08-23 05:08:44 +0530

    first commit

---
+++ src/config.py
@@ -0,0 +1,5 @@
+OPEN_AI_API = "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"
+FAISS_INDEXER_DIR = ""
+TEMP_DATA_DIR = "./temp_data"
+EMBD_MODEL_NAME = "sentence-transformers/paraphrase-albert-small-v2"
+SUMMARY_FEATURES = ["technical skills", "Education_background", "Relavent Experiance"]
